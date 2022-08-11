e940a48ea7fdd945e4cf11710cbb07f93b2b8e1c
Thabang <makgwalet@gmail.com>
2022-08-11 11:46:09 +0200

    commit and push

--- src/za/co/kaili/generate/Main.java
+++ src/za/co/kaili/generate/Main.java
@@ -1,40 +1,41 @@
+package za.co.kaili.generate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("unique password ="+generatePassword());
+        System.out.println("unique password =" + generatePassword());
 
-        String otpSting  =generateOTP();//function calling
-        System.out.println("OTP : "+otpSting);
+        String otpSting = generateOTP();//function calling
+        System.out.println("OTP : " + otpSting);
     }
 
     public static long Code() //this code returns the  unique 16 digit code
     { //creating a 16 digit code using Math.random function
-        long code = (long) ((Math.random()*9*Math.pow(10,15))+Math.pow(10,15));
-        return code;
+        return (long) ((Math.random() * 9 * Math.pow(10, 15)) + Math.pow(10, 15));
     }
 
-    public static String generatePassword(){
+    public static String generatePassword() {
         long code = Code();
         String uniquePassword = "";
 
-        for (long i=code;i !=0; i/=100) //a loop extracting 2 digits from the code
+        for (long i = code; i != 0; i /= 100) //a loop extracting 2 digits from the code
         {
-            long digit = i%100; //extracting two digits
-            if (digit <=90)
-                digit+=32;
+            long digit = i % 100; //extracting two digits
+            if (digit <= 90)
+                digit += 32;
 
             //converting those two digits(ascii value) to its character value
             char ch = (char) digit;
             // adding 32 so that our least value be a valid character
-            uniquePassword = ch+uniquePassword;
+            uniquePassword = ch + uniquePassword;
         }
         return uniquePassword;
     }
 
-    public static String generateOTP(){
+    public static String generateOTP() {
         //int randomPin declared to store the otp
         //since we using Math.random() hence we have to type cast it int
         //because Math.random() returns decimal value
-        int randomPin = (int) ((Math.random()*9000)+1000);
+        int randomPin = (int) ((Math.random() * 9000) + 1000);
         String otp = String.valueOf(randomPin);
         return otp;
     }
